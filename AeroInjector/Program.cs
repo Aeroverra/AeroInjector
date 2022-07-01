f7ccb2d5bea3d977273a65b99da3e1eed0d9af0c
Aeroverra <88tontos@gmail.com>
2022-06-30 21:49:14 -0500

    Initial Code Console App

---
+++ AeroInjector/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AeroInjector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
