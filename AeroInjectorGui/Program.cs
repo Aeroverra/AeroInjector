7569233bd9796fcb7bb319a6563fb233d077e85e
Aeroverra <88tontos@gmail.com>
2022-07-01 17:22:45 -0500

    Added Gui App with Blazor and Photino

---
+++ AeroInjectorGui/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+
+namespace Tech.Aerove.AeroInjector.Gui
+{
+
+    /// <summary>
+    /// This gui is build with Blazor and Photino from this example repo
+    ///https://github.com/tryphotino/photino.Blazor
+    ///Eventually it would be nice to use Maui once Microsoft allows you to easily publish a self contained version
+    /// </summary>
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+            appBuilder.Services
+                .AddLogging();
+
+            // register root component and selector
+            appBuilder.RootComponents.Add<App>("app");
+
+            var app = appBuilder.Build();
+
+            // customize window
+            app.MainWindow
+                .SetIconFile("favicon.ico")
+                .SetTitle("Photino Blazor Sample");
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+            {
+                app.MainWindow.OpenAlertWindow("Fatal exception", error.ExceptionObject.ToString());
+            };
+
+            app.Run();
+
+        }
+    }
+}
