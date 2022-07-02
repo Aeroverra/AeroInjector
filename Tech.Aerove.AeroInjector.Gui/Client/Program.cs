1ee77dba6e726b4c011a251b784b6af4b1e4faf3
Aeroverra <88tontos@gmail.com>
2022-07-02 15:44:07 -0500

    New Blazor Web assembly project

---
+++ Tech.Aerove.AeroInjector.Gui/Client/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Tech.Aerove.AeroInjector.Gui;
+
+namespace Tech.Aerove.AeroInjector.Gui
+{
+    [STAThread]
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
